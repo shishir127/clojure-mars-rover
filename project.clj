2ce2c29d70690701bc78b2624d50efa83cb245f6
shishir127 <shishir127@gmail.com>
2018-01-01 23:21:54 +0530

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject mars-rover "0.1.0-SNAPSHOT"
+  :description "Mars rover toy problem"
+  :url "http://shishir127.github.io"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main mars-rover.core)
