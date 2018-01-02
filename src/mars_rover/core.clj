2bd5365ae4c34ba40146a25dce522dd42be4ff8b
shishir127 <shishir127@gmail.com>
2018-01-02 16:25:51 +0530

    Added basic REPL

--- src/mars_rover/core.clj
+++ src/mars_rover/core.clj
@@ -1,10 +1,18 @@
 (ns mars-rover.core
-	(:require [mars-rover.rover :as rover])
+	(:require [mars-rover.rover :as rover]
+			  		[clojure.string :as string]
+	)
 )
 
 (defn -main
   "Runs the mars rover simulator"
   [& args]
   (println "Starting the Mars rover simulator")
-  (println (str "The rover is now at " (rover/get-location) " and facing " (rover/get-orientation)))
+  (println (format "The rover is now at %s and facing %s" (rover/get-location) (rover/get-orientation)))
+  (def continue-program true)
+  (while continue-program
+  	(def user-input (string/trim (read-line)))
+		(println user-input)
+		(def continue-program false)
+  )
 )
--- src/mars_rover/rover.clj
+++ src/mars_rover/rover.clj
@@ -2,17 +2,14 @@
 
 (def co-ordinates {:X 0 :Y 0})
 
-(def orientation "N")
+(def orientation :N)
 
 (defn get-location []
 	(str (get co-ordinates :X) ", " (get co-ordinates :Y))
 )
 
+(def directions {:N "north" :S "south" :E "east" :W "west"})
+
 (defn get-orientation []
-	(case orientation
-		"N" "north"
-		"S" "south"
-		"E" "east"
-		"W" "west"
-	)
+	(get directions orientation)
 )
